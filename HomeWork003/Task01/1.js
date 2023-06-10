71049cc71191c4427c6374e5c33ad4b64dc1be9a
aleksandra <alexandra.bulgak@gmail.com>
2023-06-10 11:19:44 +0200

    HW3 fixed

--- HomeWork003/Task01/1.js
+++ HomeWork003/Task01/1.js
@@ -1,2 +1,3 @@
-const celsius = parseFloat(prompt("Введите температуру в градусах Цельсия"));
-alert(`Цельсий: ${celsius}, Фаренгейт: ${Math.round(((9 / 5) * celsius + 32) * 10) / 10}`);
+const celsius = Number.parseFloat(prompt("Введите температуру в градусах Цельсия"));
+const fahrenheit = Math.round(((9 / 5) * celsius + 32) * 10) / 10
+alert(`Цельсий: ${celsius}, Фаренгейт: ${fahrenheit}`);
